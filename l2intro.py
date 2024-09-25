213fa01faabdd875754eed1510e1f9f3cdf994ee
kaakchan <moeook18@gmail.com>
2024-09-26 00:04:49 +0630

    add:new

---
+++ l2intro.py
@@ -0,0 +1,29 @@
+print("hello Sir")
+
+#A
+
+# + addition
+# - Subtract
+# * Multiple
+# / Division 
+# % Modulus
+
+# ** Exponentiation
+
+# print(20+5)
+#print(20-5)
+#print(10*5)
+#print(20/5)
+#print(18//5)
+# print(20%4)
+# print(2 ** 5)
+
+
+# => Operator Precedence (PEMDAS)
+# Parentheses
+# Exponent
+# Multiplication Division (fristone,firstcal)
+# Addition Subtration (fristin,firstcal)
+
+
+
