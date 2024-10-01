583987c7415b64681cc183ca73330b7da7de512b
Karakchan <152668204+Karakchan@users.noreply.github.com>
2024-10-01 23:47:57 +0700

    Add files via upload

---
+++ l3statement.py
@@ -0,0 +1,46 @@
+if True:
+    print('Yes')
+else:
+    print('No') 
+
+
+
+if False:
+    print('Yes')
+else:
+    print('No') 
+
+
+if 5 == 5   :
+    print('Yes')
+else:
+    print('No') 
+
+
+if 5 != 5:
+    print('Yes')
+else:
+    print('No')
+
+
+if 5 != 5:
+    print('Yes')
+else:
+    print('No')  
+
+
+if 5 > 1:
+    print('Yes')
+else:
+    print('No') 
+
+#Comparison Operator
+# =
+# !=
+# >
+# <
+# >=
+# <=
+# is, is not Identity Operators
+# in,not in Membership Operators
+  
